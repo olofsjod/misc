a8dfa249039c67e76d89126deb182b7310f5caaa
Olof Sjödin <me@olofsjodin.se>
2017-03-26 00:45:11 +0100

    add farenheit to celsius example code

---
+++ C/Farenheit-Celsius/farcel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Example 1.2 page 9
+// Build: cc farcel.c
+
+int main(int argc, char const *argv[]) {
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32)/9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    return 0;
+}
