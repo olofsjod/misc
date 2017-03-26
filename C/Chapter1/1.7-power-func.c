b9b1c02a2d1581adfd8ff90b1858b88fcea2a8ed
Olof Sjödin <me@olofsjodin.se>
2017-03-27 00:10:24 +0200

    add function example

---
+++ C/Chapter1/1.7-power-func.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int power(int m, int n); // this is a function prototype
+
+int main(int argc, char const *argv[]) {
+    int i;
+
+    for (i = 0; i < 10; ++i)
+        printf("%d %d %d\n", i, power(2,i), power(-3,i));
+    return 0;
+}
+
+/* power: raise base to n-th power; n >= 0 */
+int power(int base, int n) {
+    int i, p;
+
+    p = 1;
+    for (i = 1; i <= n; ++i)
+        p = p * base;
+    return p;
+}
