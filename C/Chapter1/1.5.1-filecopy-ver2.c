911c6ab0931f9b4d1518b33eb0f3408e9e9446de
Olof Sjödin <me@olofsjodin.se>
2017-03-26 03:10:25 +0200

    add useful comment

--- C/Chapter1/1.5.1-filecopy-ver2.c
+++ C/Chapter1/1.5.1-filecopy-ver2.c
@@ -10,5 +10,10 @@ int main(int argc, char const *argv[]) {
     */
     while ((c = getchar()) != EOF)
         putchar(c); // output
+    /*
+        "The precedence of != is higher than of =, which means that in the
+        absence of parentheses the relational test != would be done before the
+        assignment =."
+    */
     return 0;
 }
