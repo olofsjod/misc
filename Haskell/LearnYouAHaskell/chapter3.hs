a5b2bd4d28e1ced366b59a36339b5bf3dd07fc7f
Olof Sjödin <me@olofsjodin.se>
2017-04-06 00:37:45 +0200

    add more examples from chap 3

--- Haskell/LearnYouAHaskell/chapter3.hs
+++ Haskell/LearnYouAHaskell/chapter3.hs
@@ -13,7 +13,7 @@ sayMe y = "Not between one and two"
 
 factorial1 :: (Integral a) => a -> a
 factorial1 0 = 1
-factorial1 n = n * (factorial (n-1))
+factorial1 n = n * (factorial1 (n-1))
 
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors a b = (fst a + fst b, snd a + snd b)
@@ -31,15 +31,61 @@ third (_, _, x) = x
 head' :: [a] -> a
 head' [] = error "no you cant do that!"
 head' (x:_) = x
--- page 38
 
--- Guards
+-- Guards, Where
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight"
+    | bmi <= normal = "Normal"
+    | bmi <= fat    = "You're fat! Lose some weight"
+    | otherwise     = "Youre a whale"
+    where
+        bmi = weight / height ^ 2
+        (skinny, normal, fat) = (18.5, 25.0, 30.0)
+    {-where
+        bmi = weight / height ^ 2
+        skinny = 18.5
+        normal = 25.0
+        fat = 30.0
+    -}
 
--- Where
+{-
+This example won't work. Because where bindings aren't
+shared across function bodies of different patterns.
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
+    where
+        niceGreeting = "Hello <3"
+        badGreeting = "Oh. Pfft, it's you."
+-}
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where
+        (f:_) = firstname
+        (l:_) = lastname
+
+-- Anonymous functions in Haskell
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
 
 -- Let
+cylinder :: Double -> Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in  sideArea + 2 * topArea
 
--- page 48
+-- Same thing as:
+cylinder' :: Double -> Double -> Double
+cylinder' r h = sideArea + 2 * topArea
+    where
+        sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+
 
 -- Case expression
 
