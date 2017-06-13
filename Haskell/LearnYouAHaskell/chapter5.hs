dfbe3488899d2082c3304c431a635cf180c7e8db
Olof Sjödin <me@olofsjodin.se>
2017-06-13 22:26:18 +0200

    update chap5

--- Haskell/LearnYouAHaskell/chapter5.hs
+++ Haskell/LearnYouAHaskell/chapter5.hs
@@ -56,3 +56,83 @@ quicksort' (x:xs) =
 largestDivisible :: Integer
 largestDivisible = head (filter p [100000,99999..])
     where p x = mod x 3829 == 0
+
+{-
+Implementation of Collatz sequence
+-}
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+  | even n = n:chain (div n 2)
+  | odd n = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+  where isLong xs = length xs > 15
+
+{-
+ It is also possible to map with functions than requires more than one parameters. 
+-}
+
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+lambdaTest :: [Int]
+lambdaTest = map (\x -> x + 3) [1,2,3,4]
+
+lambdaMoreParam :: [Int]
+lambdaMoreParam = zipWith (\x y -> x + y) [1, 2, 3, 4] [4, 3, 2, 1]
+
+lambdaMoreParam2 :: [Int]
+lambdaMoreParam2 = map (\(x, y) -> x + y) [(1,4), (2,3), (3,2), (4,1)]
+
+-- NOTE If a pattern match fails in a lambda, a runtime error will occur
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+addThree' :: Int -> Int -> Int -> Int
+addThree' = \x -> \y -> \z -> x + y + z
+
+-- These two are equivalent due to currying
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
+
+-- The equivalent function with foldl is
+map''' :: (a -> b) -> [a] -> [b]
+map''' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+{- Function Composition -}
+funcComp :: [Int]
+
+-- funcComp = map (\x -> negate (abs x)) [5, -3, -6, 7]
+-- same as
+funcComp = map (negate . abs) [5, -3, -6, 7]
+
+-- Multiparameter function composition
+funcComp2 :: Int
+funcComp2 = sum . replicate 4 $ max 4.5 2
+
+-- Dollar has lower rank than space, sum . replicate 4 returns a partial applied function that operates on the result of max 4.5 2
+
+{- Example of a function written in "point-free style" -}
+sum''' :: (Num a) => [a] -> a
+sum''' = foldl (+) 0
+
+{- Another example. Here we can see the power of function composition:
+fn x = ceiling (negate (tan (cos (max 50 x))))
+is equivalent to
+fn' = ceiling . negate . tan . cos . max 50
+-}
+
+
+oddSquareSum :: Integer
+oddSquareSum = sum . takeWhile (<10000) . filter odd . map (^2) [1..]
