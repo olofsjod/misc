95b2173f8f7dc8d64fd148d2e549970c14d632e2
Olof Sjödin <me@olofsjodin.se>
2017-06-13 23:51:05 +0200

    update chap 6

--- Haskell/LearnYouAHaskell/chapter6.hs
+++ Haskell/LearnYouAHaskell/chapter6.hs
@@ -3,7 +3,7 @@ import Data.List
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 
--- I don't understand... Why not Num a?
+-- 'Eq a' is like the Comparable interface in Java
 isNeedleInHay :: (Eq a) => [a] -> [a] -> Bool
 isNeedleInHay n h = foldl (||) False . map (isPrefixOf n) . tails $ h
 
