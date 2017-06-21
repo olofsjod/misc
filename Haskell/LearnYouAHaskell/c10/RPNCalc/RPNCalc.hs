39a6eac77e7a5cbc42dc102892d90f783dadf443
Olof Sjödin <me@olofsjodin.se>
2017-06-21 04:57:41 +0200

    add rpn calculator

---
+++ Haskell/LearnYouAHaskell/c10/RPNCalc/RPNCalc.hs
@@ -0,0 +1,11 @@
+main = do
+  calcThis <- getLine
+  print $ solveRPN calcThis
+
+solveRPN :: String -> Double
+solveRPN = head . foldl foldingFunction [] . words
+  where foldingFunction (x:y:ys) "*" = (y * x):ys
+        foldingFunction (x:y:ys) "+" = (y + x):ys
+        foldingFunction (x:y:ys) "-" = (y - x):ys
+        foldingFunction xs numberString = read numberString:xs
+        
