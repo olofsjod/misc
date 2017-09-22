f616c9ba32e4c95ff10e9289893a0bd6644633cf
Olof Sjödin <me@olofsjodin.se>
2017-09-22 20:17:07 +0200

    replace main with interact

--- Haskell/LearnYouAHaskell/c09/shortlinesonly.hs
+++ Haskell/LearnYouAHaskell/c09/shortlinesonly.hs
@@ -1,6 +1,5 @@
-main = do
-  contents <- getContents
-  putStr (shortLinesOnly contents)
+main = interact shortLinesOnly
+
 
 shortLinesOnly :: String -> String
 shortLinesOnly = unlines . filter (\line -> length line < 10) . lines
