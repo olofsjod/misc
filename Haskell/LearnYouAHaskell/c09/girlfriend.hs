63f6535903d3d594fde944a23c707f27aaf004cd
Olof Sjödin <me@olofsjodin.se>
2017-09-22 22:19:09 +0200

    replace with readFile

--- Haskell/LearnYouAHaskell/c09/girlfriend.hs
+++ Haskell/LearnYouAHaskell/c09/girlfriend.hs
@@ -1,7 +1,6 @@
 import System.IO
 
 main = do
-  withFile "girlfriend.txt" ReadMode (\handle -> do
-                                         contents <- hGetContents handle
-                                         putStr contents)
+  contents <- readFile "girlfriend.txt"
+  putStr contents
 
