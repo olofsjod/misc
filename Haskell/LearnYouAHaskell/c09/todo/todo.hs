bdacb2e3b34739495130887ab1e314204ba820ea
Olof Sjödin <me@olofsjodin.se>
2017-09-22 23:10:43 +0200

    update todo program with arguments

--- Haskell/LearnYouAHaskell/c09/todo/todo.hs
+++ Haskell/LearnYouAHaskell/c09/todo/todo.hs
@@ -1,6 +1,43 @@
 import System.IO
+import System.Directory
+import System.Environment
+import Data.List
+import Control.Exception
 
 main = do
-  putStrLn "What to do?"
-  todoItem <- getLine
-  appendFile "todo.txt" (todoItem ++ "\n")
+  (command:argList) <- getArgs
+  dispatch command argList
+  
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+  putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, todoItem] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+
+      number = read todoItem
+      newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+
+  bracketOnError (openTempFile "." "temp") (\(tempName, tempHandle) -> do
+                                               hClose tempHandle
+                                               removeFile tempName) (\(tempName, tempHandle) -> do
+                                                                        hPutStr tempHandle newTodoItems
+                                                                        hClose tempHandle
+                                                                        removeFile "todo.txt"
+                                                                        renameFile tempName "todo.txt")
+  
+  
