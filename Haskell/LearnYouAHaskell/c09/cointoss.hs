1b9fefcb619ccebe1a01ba9754e031aecc77f41b
Olof Sjödin <me@olofsjodin.se>
2017-09-23 14:26:40 +0200

    add coin toss example

---
+++ Haskell/LearnYouAHaskell/c09/cointoss.hs
@@ -0,0 +1,7 @@
+import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen = let (firstCoin, newGen) = random gen
+                     (secondCoin, newGen') = random newGen
+                     (thirdCoin, newGen'') = random newGen'
+                 in (firstCoin, secondCoin, thirdCoin)
