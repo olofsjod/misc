cac3b560f5ce758ba551d185f75afdb476efec13
Olof Sjödin <me@olofsjodin.se>
2022-04-11 12:15:22 +0200

    add solution to Q0.1

---
+++ Cpp/AcceleratedCpp/0chap/1/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << 3 + 4 << std::endl; // Evaluates take two integers and applies
+                                   // the binary '+' operator on it, returning one integer
+}
