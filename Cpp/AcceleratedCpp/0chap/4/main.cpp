e6bfcc5813403427a0ebe279751d4e8904dec7f3
Olof Sjödin <me@olofsjodin.se>
2022-04-15 10:48:44 +0200

    add solution to Q0.4

---
+++ Cpp/AcceleratedCpp/0chap/4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout <<
+    "#include <iostream>\n"
+    "\n"
+    "int main() {\n"
+    "\tstd::cout << \"Hello world!\\n\" << std::endl;\n"
+    "\treturn 0;\n"
+    "}" << std::endl;
+  return 0;
+}
